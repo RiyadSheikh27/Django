855b9847a10c53480187f4f260fc4b5f0bf12b83
MD. Fazle Rabbi Riyad <riyad.cse27@gmail.com>
2025-02-09 01:32:55 +0600

    Implement Static Files in Django

--- WsCube Tech/first_project/first_project/views.py
+++ WsCube Tech/first_project/first_project/views.py
@@ -8,14 +8,14 @@ def courseDetails(request,courseId):
     return HttpResponse(courseId)
 
 def homePage(request):
-    data={
-        'title':'Test Home Page',
-        'bdata':'Welcome to Homepage',
-        'clist':['Java ','C++ ','Python '],
-        'numbers':[10,20,30,40,50],
-        'std_det_dic':[
-            {'name':'Riyad','phn':'016120000'},
-            {'name':'Abir','phn':'013070000'}
-        ]
-    }
-    return render(request,"index.html",data)
+    # data={
+    #     'title':'Test Home Page',
+    #     'bdata':'Welcome to Homepage',
+    #     'clist':['Java ','C++ ','Python '],
+    #     'numbers':[10,20,30,40,50],
+    #     'std_det_dic':[
+    #         {'name':'Riyad','phn':'016120000'},
+    #         {'name':'Abir','phn':'013070000'}
+    #     ]
+    # }
+    return render(request,"index.html")
