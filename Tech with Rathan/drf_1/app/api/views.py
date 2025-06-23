a15a42563321dc33545ba2c23ff99f10af60b133
MD. Fazle Rabbi Riyad <riyad.cse27@gmail.com>
2025-06-23 23:58:34 +0600

    Simple GET API design

--- Tech with Rathan/drf_1/app/api/views.py
+++ Tech with Rathan/drf_1/app/api/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET'])
 def studentView(request):
-    students = Student.objects.all().values()
-    students_list = list(students)
-    return JsonResponse(students_list, safe=False)
+      if request.method == 'GET':
+            # Get all the data from the student table
+            students = Student.objects.all()
+            serializer = StudentSerializer(students, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
